6c6ad53427f1929c25e8bccb27774b8ead95b4cc
Joey R. Hernandez Perez <joeyrhernandez1@fb.com>
2020-06-24 23:08:16 -0400

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by Joey R. Hernandez Perez on 6/24/20.
+//  Copyright © 2020 joeyrhernandez1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Flix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flix
+//
+//  Created by Joey R. Hernandez Perez on 6/24/20.
+//  Copyright © 2020 joeyrhernandez1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
