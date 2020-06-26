4d1ee88ca40f3f08f02b1c844af6abf2b6739b49
Joey R. Hernandez Perez <joeyrhernandez1@fb.com>
2020-06-25 23:38:29 -0400

    User can see a collection view of movie posters by tapping a tab bar button.

---
+++ Flix/Views/MovieCollectionCell.m
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionCell.m
+//  Flix
+//
+//  Created by Joey R. Hernandez Perez on 6/25/20.
+//  Copyright © 2020 joeyrhernandez1. All rights reserved.
+//
+
+#import "MovieCollectionCell.h"
+
+@implementation MovieCollectionCell
+
+@end
